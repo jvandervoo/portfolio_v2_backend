ad4284d6f79d9e84324178e4a1e9a43ef918464b
Unknown <andretuparamorrison@gmail.com>
2018-12-23 20:54:26 +1300

    Created GetProjects lambda

---
+++ index.js
@@ -0,0 +1,60 @@
+const AWS = require("aws-sdk");
+AWS.config.region = "ap-southeast-2";
+const DynamoDB = new AWS.DynamoDB();
+
+exports.handler = async (event, context, callback) => {
+    try {
+        let id;
+        if (event.pathParameters) {
+            id = event.pathParameters.id;
+        }
+
+        var params = {
+            ExpressionAttributeNames: {
+                "#ID": "id",
+                "#T": "title"
+            },
+            ProjectionExpression: "#ID, #T",
+            TableName: "Projects"
+        };
+
+        if (id) {
+            params.ExpressionAttributeValues = {
+                ":id": {
+                    N: id
+                }
+            };
+            params.FilterExpression = "id = :id";
+        }
+
+        const dynamoResponse = await DynamoDB.scan(params).promise();
+
+        const projects = [];
+
+        if (dynamoResponse.Items) {
+            dynamoResponse.Items.forEach(item => {
+                projects.push({
+                    id: item.id.N,
+                    title: item.title.S
+                });
+            });
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(projects),
+        };
+
+        callback(null, response);
+    } catch (e) {
+        const responseBody = {
+            errorMessage: `Something went wrong: ${e.message}`
+        };
+        const response = {
+            statusCode: 500,
+            body: JSON.stringify(responseBody)
+        }
+
+        callback(null, response);
+    }
+};
