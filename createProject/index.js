8b0e4215a3f513ac7c8713d04df342774c31af07
Jasper Van der Voo <jasper.voo@gmail.com>
2019-02-04 21:19:59 +1300

    Added support for Project Git Links

--- createProject/index.js
+++ createProject/index.js
@@ -1,73 +1,77 @@
 const AWS = require("aws-sdk");
 AWS.config.region = "ap-southeast-2";
 const DynamoDB = new AWS.DynamoDB({
-    apiVersion: '2012-08-10'
+    apiVersion: "2012-08-10",
 });
 
 exports.handler = async (event, context, callback) => {
     try {
         const requestBody = JSON.parse(event.body);
         if (!(requestBody.title && requestBody.description && requestBody.date)) {
-            throw Error('Invalid Request');
+            throw Error("Invalid Request");
         }
         const id = createId(requestBody.title);
 
         var params = {
             Item: {
-                "ID": {
-                    S: id
+                ID: {
+                    S: id,
                 },
-                "Title": {
-                    S: requestBody.title
+                Title: {
+                    S: requestBody.title,
                 },
-                "Description": {
-                    S: requestBody.description
+                Description: {
+                    S: requestBody.description,
                 },
-                "Date": {
-                    S: requestBody.date
+                Date: {
+                    S: requestBody.date,
                 },
             },
-            TableName: "Projects"
+            TableName: "Projects",
         };
 
         if (requestBody.img) {
-            params.Item['ImageLink'] = {
-                S: requestBody.img
+            params.Item["ImageLink"] = {
+                S: requestBody.img,
+            };
+        }
+        if (requestBody.git) {
+            params.Item["GitLink"] = {
+                S: requestBody.git,
             };
         }
 
         const dynamoResponse = await DynamoDB.putItem(params).promise();
 
         const responseBody = {
-            message: 'Successfully created project',
-            id: id
+            message: "Successfully created project",
+            id: id,
         };
         const response = {
             statusCode: 200,
             body: JSON.stringify(responseBody),
             headers: {
-                "Access-Control-Allow-Origin": "*"
-            }
+                "Access-Control-Allow-Origin": "*",
+            },
         };
         callback(null, response);
     } catch (e) {
         const responseBody = {
-            errorMessage: `Something went wrong: ${e.message}`
+            errorMessage: `Something went wrong: ${e.message}`,
         };
         const response = {
             statusCode: 500,
             body: JSON.stringify(responseBody),
             headers: {
-                "Access-Control-Allow-Origin": "*"
-            }
-        }
+                "Access-Control-Allow-Origin": "*",
+            },
+        };
 
         callback(null, response);
     }
-}
+};
 
 function createId(title) {
-    const idUnencoded = title.replace(' ', '-').toLowerCase();
+    const idUnencoded = title.replace(" ", "-").toLowerCase();
     return encodeURIComponent(idUnencoded);
-
 }
